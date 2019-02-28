40752e2cf92f2f4fabc24843fb7afe615d4ee508
Fei Liu <uscliufei@gmail.com>
2019-02-27 16:13:53 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,2 +1,106 @@
 #include "main.h"
 
+VM:: VM(){
+	this->avaiable_time = 0;
+}
+
+VM:: ~VM(){};
+
+void VM:: updateTime(int period){
+	this->avaiable_time += period;
+}
+
+
+
+Server:: Server(){
+	this->vm_num = 4;
+}
+
+Server:: Server(int vm_num){
+	v = new VM[vm_num];
+	this->vm_num = vm_num;
+}
+
+Server:: ~Server(){};
+
+void Server:: configVM(){
+	double fractions_cpu[vm_num + 1];
+	fractions_cpu[0] = 0;
+	for(int i = 1; i < vm_num; i++){
+		while(true){
+			double r = rand() / double(RAND_MAX);
+			if(r >= 0.1 && r <= 0.4){
+				fractions_cpu[i] = r;
+				break;
+			}
+		}
+	}
+
+	double fractions_memory[vm_num + 1];
+	for(int i = 1; i < vm_num; i++){
+		while(true){
+			double r = rand() / double(RAND_MAX);
+			if(r >= 0.1 && r <= 0.4){
+				fractions_memory[i] = r;
+				break;
+			}
+		}
+	}
+
+	fractions_memory[vm_num] = 1;
+	fractions_cpu[vm_num] = 1;
+	sort(fractions_memory, fractions_memory + vm_num);
+	sort(fractions_cpu, fractions_cpu + vm_num);
+	for(int i = 0; i < vm_num; i++){
+		v[i].cpu_frac = fractions_cpu[i + 1] - fractions_cpu[i];
+		v[i].memory_frac = fractions_memory[i + 1] - fractions_memory[i];
+	}
+}
+
+ServerFarm:: ServerFarm(){
+	this->server_num = 10;
+}
+
+
+ServerFarm:: ServerFarm(int server_num){
+	this->server_num = server_num;
+	s = new Server[server_num];
+}
+
+void ServerFarm::  configServer(int farm_id, int vm_num){ 
+	for(int i = 0; i < server_num; i++){
+		Server cur_vm(vm_num) ;
+		s[i] = cur_vm;
+		s[i].farm_id = farm_id;
+	}
+}
+
+// Job:: Job(int job_id, int task_num){
+// 	this->job_id = job_id;
+// 	this->task_num = task_num;
+// 	t = new Task[task_num];
+// }
+
+// void Job:: configJob(){
+// 	for(int i = 0; i < task_num; i++){
+// 		t[i].job_id = job_id;
+// 	}
+// }
+
+int main(int argc, char const *argv[]) {
+	ServerFarm *test = new ServerFarm(10);
+	test->configServer(1, 4);
+	test->farm_id = 1;
+	// test->server_num = 4;
+	cout<< "farmid " << test->farm_id << " servernum " << test->server_num << endl; 
+	for(int i = 0; i < test->server_num; i++){
+		test->s[i].configVM();
+		for(int j = 0; j < test->s[i].vm_num; j++){
+			cout << test->s[i].v[j].cpu_frac << "  "  << test->s[i].v[j].memory_frac << endl;
+		}
+		cout << "===========" << endl;
+	}
+
+	return 0;
+}
+
--- main.h
+++ main.h
@@ -1,4 +1,3 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -7,52 +6,72 @@
 #include <cstring>
 #include <cstdlib>
 #include <sstream>
+#include <math.h>
+#include<algorithm>
+
 using namespace std;
-
-class ServerFarm{
-public:
-	Server *s;
-	int server_num;
- 	Farm();
- 	Farm()
- 	~Farm();
-};
-
-class Server {	
-	public:
-	int vm_num;
- 	
- 	const int cpu_total = 1;
- 	const int memory_total = 1;
- 	Server();
- 	~Server();
-};
 
 class VM {
 public:
- 	int vm_type;
- 	int cpu_frac;
- 	int ready_time;
- 	int memory_frac;
+ 	double cpu_frac;
+ 	double memory_frac;
+ 	int avaiable_time;
  	VM();
  	~VM();
+ 	void updateTime(int);
 };
 
-class Job {
+class Server {	
 public:
- 	Task *t;
- 	int task_num;
- 	Job();
- 	~Job();
+	VM *v;
+	int vm_num;
+ 	int farm_id;
+ 	// const int cpu_total = 1;
+ 	// const int memory_total = 1;
+ 	Server();
+ 	Server(int vm_num);
+ 	~Server();
+ 	void configVM();
 };
 
-class Task {
+class ServerFarm {
 public:
- 	float core;
- 	float memory;
- 	Task();
- 	~Task();
- 
+	Server *s;
+	int farm_id;
+	int server_num;
+ 	ServerFarm();
+ 	ServerFarm(int server_num);
+ 	~ServerFarm();
+ 	void configServer(int farm_id, int vm_num);
 };
+
+
+// class Task {
+// public:
+// 	int job_id;
+// 	int start_time;
+// 	int exe_period;
+// 	int task_type;
+//  	float cpu_usage;
+//  	float memory_usage;
+//  	Task();
+//  	Task(int start_time, int job_id, int task_type, float cpu_usage, float memory_usage, int exe_period);
+//  	~Task();
+// };
+
+
+// class Job {
+// public:
+//  	Task *t;
+//  	int deadline;
+//  	int task_num;
+//  	int job_id;
+//  	Job();
+//  	Job(int job_id, int task_num);
+//  	~Job();
+//  	void configJob();
+// };
+
+
 
 #endif
